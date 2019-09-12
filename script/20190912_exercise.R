241809678dba0dd86ca0892fcbfe81e8be0d3aef
aswadi <aswadi1986@gmail.com>
2019-09-12 19:58:08 +0800

    update

--- script/20190912_exercise.R
+++ script/20190912_exercise.R
@@ -1,6 +1,55 @@
+### 2019/9/12 - Introduction to R
+### 
+
+# code
 x <- 1
 print(x)
 x
 msg <- "hello"
-x <- 
+x <-  #incomplete
   
+x <- 11:30
+x
+
+
+y <- vector("numeric", length=10)
+x <- vector()
+x
+
+y <- c(1.7,"a")
+y <- c(TRUE,2)
+y <- c("a",TRUE)
+
+x <- 0:6
+class(x)
+as.numeric(x)
+as.logical(x)
+as.character(x)
+
+series <- 1:10
+series <- seq(10)
+series <- seq(1,10,0.1)
+
+x <- seq(0,10,2)
+x[3] <- 100
+
+x[c(2,4)]
+x[-1]
+x[c(2,-4)]
+x[c(2.4, 3.5)]
+
+x <- NULL
+remove(x)
+x <- 5
+y <- 2
+x^y
+x%%y
+
+x <- c(1:10)
+x[(x>8)|(x<5)]
+x%/%y
+
+rep(c(0,0,7), times=4)
+rep(c(2,4,2), each=2)
+rep(c(0,7), times=c(4,3))
+rep(1:3, length.out=9)
